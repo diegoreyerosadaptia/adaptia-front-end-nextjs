72ab3c2bc51b4a84399ec4521b3cf5cf86733442
IgnacioGaute <nachitogaute@gmail.com>
2025-12-05 11:37:18 -0300

    fix: confirm password

--- src/actions/auth/reset-password.action.ts
+++ src/actions/auth/reset-password.action.ts
@@ -18,11 +18,10 @@ export async function resetPasswordAction(values: ResetPasswordSchemaType) {
 
     const supabase = await createClient();
     if (!supabase) {
-        return { error: { message: "Supabase no configurado" } };
-      }
+      return { error: { message: "Supabase no configurado" } };
+    }
 
     // ✅ 1) Chequeo de usuario en tu tabla local
-    // Cambiá "users" y/o el campo si tu esquema es distinto
     const { data: existingUser, error: userErr } = await supabase
       .from("users")
       .select("id, email")
@@ -39,12 +38,15 @@ export async function resetPasswordAction(values: ResetPasswordSchemaType) {
     }
 
     // ✅ 2) Envío email Supabase
-    const redirectTo =
-    "https://adaptia-front-end-nextjs.vercel.app/auth/new-password"
-  
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo,
-  })
+    const base = (process.env.NEXT_PUBLIC_SITE_URL ?? "").replace(/\/$/, "");
+
+    const redirectTo = `${base}/auth/new-password`;
+
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo,
+    });
+
+
     if (error) {
       console.error("Supabase resetPasswordForEmail error:", error);
       return { error: "Error al enviar el email de recuperación" };
